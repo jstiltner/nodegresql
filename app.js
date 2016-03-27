cb04fcb7490d50edf2b174af3b91a3b03f1cefba
Jason Stiltner <jasonstiltner@gmail.com>
2016-03-26 19:31:26 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('postgres://localhost:5432/nodegresql');
+
+const Frienemy = sequelize.define('Frienemy', {
+  name: Sequelize.STRING,
+  birthday: Sequelize.DATE,
+  friend: Sequelize.BOOLEAN
+});
+const Project = sequelize.define('projects', {
+  name: Sequelize.STRING
+});
+
+Project.hasMany(Frienemy);
+
+let jane;
+
+sequelize.sync().then(() => {
+  jane = Frienemy.create({
+    name: 'Jane Doe',
+    birthday: new Date('1980-3-4')
+  });
+}).then(function(frienemy) {  //eslint-disable-line
+  console.log(jane);
+}).then(() => {
+  return Project.create({
+    name: 'Angular 101'
+  });
+}).then((project) => {  //eslint-disable-line
+  // jane.addProject(project);
+});
+
+
+
